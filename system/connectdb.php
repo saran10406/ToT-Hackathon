461861db53cda57e94f4ee41903422bdabc10ac1
saran10406 <46120244+saran10406@users.noreply.github.com>
2019-02-12 01:01:29 +0700

    Up

---
+++ system/connectdb.php
@@ -0,0 +1,18 @@
+<?php
+  date_default_timezone_set('Asia/Bangkok');
+  $today_time = date("Y-m-d h:i:sa");
+  //$today_day = date('d-m-Y H:i:s');
+  //$today_date = date("H:i:s") + ' ' + date('d-m-Y');
+
+  $ServerName = 'localhost:8080';
+  $ServerUsername = 'root';
+  $ServerPassword = 'saran1230';
+  $ServerDataBase = 'totskill';
+
+  $Connect = mysqli_connect($ServerName,$ServerUsername,$ServerPassword,$ServerDataBase);
+  mysqli_query($Connect,"SET NAMES UTF8");
+  if(!$Connect){
+    echo 'Error';
+    exit;
+  }
+ ?>
